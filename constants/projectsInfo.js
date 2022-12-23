40e7503abaff52b5ec20ec86b608c9e7d5691da7
Santiago <santiagopacini@gmail.com>
2022-12-23 16:58:10 -0300

    projects stack updated

--- constants/projectsInfo.js
+++ constants/projectsInfo.js
@@ -1,12 +1,27 @@
-import LeRestaurant from '../public/images/Projects/LeRestaurant.png';
 import CalendarApp from '../public/images/Projects/CalendarApp.png';
-import TodoApp from '../public/images/Projects/Todo-App.png';
-import WeatherApp from '../public/images/Projects/WeatherApp.png';
+import KanbanBoard from '../public/images/Projects/KanbanBoard.png';
 import BasementEcommerce from '../public/images/Projects/Basement-Home.png';
 
 const projectsInfo = [
   {
     id: 1,
+    img: KanbanBoard,
+    title: 'Kanban Clone',
+    text: `CRUD Aplicacion de tarjetas con Autenticación. Podes modificar las tareas, marcarla como completada, en progreso o borrarlas. Autenticación y Base de Datos con Firebase.`,
+    stack: [
+      'ReactJS',
+      'Typescript',
+      'ChakraUI',
+      'Redux Toolkit',
+      'React Router Dom',
+      'Firebase',
+      'Formik',
+    ],
+    repo: 'https://github.com/Santipac/KanbanBoard',
+    deploy: 'https://kanbanclone.vercel.app/',
+  },
+  {
+    id: 2,
     img: BasementEcommerce,
     title: 'Basement Ecommerce',
     text: `Tienda de ropa. El proyecto está basado en un challenge de Basement para aplicar a alguna de sus vacantes. Elegí realizarlo como temática y darle mi toque personal.`,
@@ -22,7 +37,7 @@ const projectsInfo = [
     deploy: 'https://ecommerce-basement.vercel.app/',
   },
   {
-    id: 2,
+    id: 3,
     img: null,
     title: 'RESTful-API Blog',
     text: `Backend para un Blog que está en desarrollo. CRUD con despliegue en Railway. Trabajo en local con contenedor Docker.`,
@@ -39,7 +54,7 @@ const projectsInfo = [
     deploy: null,
   },
   {
-    id: 3,
+    id: 4,
     img: CalendarApp,
     title: 'CalendarApp',
     text: `Aplicación MERN de Calendario para equipos, cada Usuario puede modificar o eliminar sus propios Eventos pero solo puede leer los de los demás. `,
@@ -54,23 +69,6 @@ const projectsInfo = [
     ],
     repo: 'https://github.com/Santipac/CalendarApp',
     deploy: 'https://calendar-app-santipac.vercel.app/',
-  },
-
-  {
-    id: 4,
-    img: TodoApp,
-    title: 'Todo App',
-    text: `CRUD Aplicacion de tareas con Autenticación. Podes modificar las tareas, marcar como completada o borrarlas. Autenticación y Base de Datos con Firebase.`,
-    stack: [
-      'ReactJS',
-      'ChakraUI',
-      'Redux Toolkit',
-      'React Router Dom',
-      'Firebase',
-      'Formik',
-    ],
-    repo: 'https://github.com/Santipac/Todo-App',
-    deploy: 'https://todo-app-santipac.vercel.app/',
   },
 ];
 export default projectsInfo;

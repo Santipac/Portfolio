ae5a647b1f411f603f979b89de6b6a322e9694ab
Santiago Pacini <santiagopacini@gmail.com>
2024-06-25 20:00:55 -0300

    removing font package

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter } from "@next/font/google";
-import localFont from "@next/font/local";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
 export const calSans = localFont({
     src: '../public/fonts/CalSans-SemiBold.ttf',
